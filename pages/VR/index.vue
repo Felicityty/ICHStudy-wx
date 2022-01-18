aba59f30168456a9bfb54d54cee18d505832a2c8
Felicity <felicity_1213@163.com>
2022-01-18 15:36:27 +0800

    icons

---
+++ pages/VR/index.vue
@@ -0,0 +1,27 @@
+<template>
+		<view class="content">
+			<view class="text-area">
+				<text class="title">{{title}}</text>
+			</view>
+		</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'VR'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
